f48bd999a27c407e432b9b5ac8aa45320ac5f8a0
Ulas Savkin <uli.will.code@protonmail.com>
2020-08-09 23:35:27 +0300

    login template created

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,76 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+<div id="app">
+    <v-app>
+      <v-app-bar app color="red" dark>
+        <v-toolbar-title>White Curtain</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text rounded>Home</v-btn>
+        <v-btn text rounded>Login</v-btn>
+      </v-app-bar>
+        <v-content>
+      <!-- Login Module -->
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title class="pb-0">
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+            label="Email"
+            prepend-icon="mdi-email"
+            />
+            <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = 
+            !showPassword"
+            />
+          </v-form>
+        </v-card-text>
+        <v-diveder></v-diveder>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="primary">Login</v-btn>
+        </v-card-actions>
+        </v-card>
+    </v-content>
+            <v-footer color="primary lighten-1" padless>
+        <v-layout justify-center wrap>
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+          >
+            {{ link }}
+          </v-btn>
+          <v-flex primary lighten-2 py-4 text-center white--text xs12>
+            {{ new Date().getFullYear() }} — <strong>White Curtain</strong>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+    </v-app>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
 
+
+<script>
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  data () {
+    return {
+      showPassword: false,
+      links: [
+        'Home',
+        'Login'
+      ]
+    }
+  }
+}
 </script>
