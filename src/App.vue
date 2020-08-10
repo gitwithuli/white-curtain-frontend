d5fc77b4bf1ff317cdffddb3661d4e89fde06d10
Ulas Savkin <uli.will.code@protonmail.com>
2020-08-10 11:14:15 +0300

    installing router

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,71 @@
 <template>
-  <div>
-    <h1>Movies</h1>
-        <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :items-per-page="5"
-        class="elevation-1"
-        >
-      </v-data-table>
+  <div id="app">
+    <v-app>
+      <v-app-bar app color="red" dark>
+        <v-toolbar-title>White Curtain</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text rounded>Home</v-btn>
+        <v-btn text rounded>Login</v-btn>
+      </v-app-bar>
+      <v-content>
+          
+        <!-- Login Module -->
+        <v-card width="400" class="mx-auto mt-5">
+          <v-card-title class="pb-0">
+            <h1 class="display-1">Login</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Email" prepend-icon="mdi-email" />
+              <v-text-field
+                :type="showPassword ? 'text' : 'password'"
+                label="Password"
+                prepend-icon="mdi-lock"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = 
+            !showPassword"
+              />
+            </v-form>
+          </v-card-text>
+          <v-diveder></v-diveder>
+          <v-card-actions>
+            <v-btn color="success">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-content>
+      <v-footer color="primary lighten-1" padless>
+        <v-layout justify-center wrap>
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+          >{{ link }}</v-btn>
+          <v-flex primary lighten-2 py-4 text-center white--text xs12>
+            {{ new Date().getFullYear() }} —
+            <strong>White Curtain</strong>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
-
-
 <script>
-  export default {
-    data () {
-      return {
-        headers: [
-          {
-            text: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
-      }
-    },
+export default {
+  name: 'App',
+  data () {
+    return {
+      showPassword: false,
+      links: [
+        'Home',
+        'Login'
+      ]
+    }
   }
+}
 </script>
