b1cffc7542c5302ca5e7a0c94e8cf1cc4a4b64e3
Ulas Savkin <uli.will.code@protonmail.com>
2020-08-10 20:14:02 +0300

    axios file created

--- src/backend/axios/index.js
+++ src/backend/axios/index.js
@@ -1,3 +1,18 @@
-import "axios" from 'axios'
+import axios from 'axios';
 
 const API_URL = 'http:localhost:3000/api/v1'
+
+// const securedAxiosInstance = axios.create({
+//     baseURL: API_URL,
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
+
+const actions = {
+    async logIn({ commit }) {
+        const response = await axios.get(API_URL + "/")
+    }
+}
+
